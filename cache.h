7c9d2a130ac8679af9af1697fdf9a11092de4148
trimpage <45026144+trimpage@users.noreply.github.com>
2019-06-07 14:14:41 -0700

    Add files via upload

--- cache.h
+++ cache.h
@@ -5,12 +5,132 @@
 
 namespace pic10b {
 
-	template <typename T, size_t N, typename CallableType = std::less<T>>
-	class cache {
+	template <typename T, size_t N, typename ord = std::less<T>>
+	class Cache {
 	public:
+		using value_type = T;
+		using reference = T&;
+		using const_reference = const T&;
 
+		constexpr Cache(ord _order) : values{}, highest(0), lowest(0), size(N), takenSpots(0), order(_order) {}
+
+		/*
+		insert function for an lvalue
+		@tparam T: data type cache stores
+		@param value: lvalue of data type to be inserted
+		*/
+		void insert(const T& value) {
+			//if cache is full
+			if (takenSpots == size) {
+				//remove front
+				values.pop_front();
+
+				//insert
+				values.push_back(value);
+			}
+			//otherwise cache not full
+			else {
+				//insert
+				values.push_back(value);
+
+				//update counter
+				++takenSpots;
+			}
+
+			//if value is ordered higher than current highest
+			if (value order highest) {
+				//replace highest
+				highest = value;
+			}
+		}
+
+		/*
+		insert function for an lvalue
+		@tparam T: data type cache stores
+		@param value: rvalue of data type to be inserted
+		*/
+		void insert(T&& value) {
+			//if cache is full
+			if (takenSpots == size) {
+				//remove front
+				values.pop_front();
+
+				//insert
+				values.push_back(value);
+			}
+			//otherwise cache not full
+			else {
+				//insert
+				values.push_back(value);
+
+				//update counter
+				++takenSpots;
+			}
+
+			//if value is ordered higher than current highest
+			if (value order highest) {
+				//replace highest
+				highest = value;
+			}
+		}
+
+		/*
+		pop function
+		*/
+		void pop() {
+			//if no elements in cache
+			if (takenSpots == 0) {
+				throw std::logic_error("Error! Cannot pop with no elements!");
+			}
+			//otherwise there are elements
+			else {
+				//pop front element
+				values.pop_front();
+
+				//decrement taken spots
+				--takenSpots;
+			}
+		}
+
+		/*
+		begin function
+		@return: returns const iterator of first element
+		*/
+		const_iterator begin() const {
+			return values.begin();
+		}
+
+		/*
+		end function
+		@return: returns const iterator of past the end element
+		*/
+		const_iterator end() const {
+			return values.end();
+		}
+
+		/*
+		function to get lowest value
+		@return: returns type T lowest value
+		*/
+		T get_low() const {
+			return lowest;
+		}
+
+		/*
+		function to get highest value
+		@return: returns type T highest value
+		*/
+		T get_high() const {
+			return highest;
+		}
+		
 	private:
-		T 
+		std::vector<T> values(N);
+		T highest;
+		T lowest;
+		size_t size;
+		size_t takenSpots;
+		ord order;
 	};
 
 
